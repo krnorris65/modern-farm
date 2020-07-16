cb503b0b1a5abc023c9e42b7aed90ddf3b8ce1f7
Kristen Norris <krnorris65@gmail.com>
2020-07-16 13:39:56 -0500

    updated harvest function

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,15 +1,13 @@
 export const harvestPlants = (seedsArray) => {
     const plantArray = []
     seedsArray.forEach(plant => {
-        if(plant.type === "Corn"){
-            const cornOutput = plant.output/2
-            for(let i=0; i<cornOutput; i++){
-                plantArray.push(plant)
-            }
-        } else {
-            for(let i=0; i<plant.output; i++){
-                plantArray.push(plant)
-            }
+        // if the plant type is corn then then output is half
+        if (plant.type === "Corn") {
+            plant.output /= 2
+
+        }
+        for (let i = 0; i < plant.output; i++) {
+            plantArray.push(plant)
         }
     })
     return plantArray
